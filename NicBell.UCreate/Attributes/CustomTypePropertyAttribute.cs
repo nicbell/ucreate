0d90b9152c75f541b82ccde78bd036a0c9c85cc4
Nic Bell <me@nicbell.net>
2014-07-16 17:18:17 +0100

    Added missing files

---
+++ NicBell.UCreate/Attributes/CustomTypePropertyAttribute.cs
@@ -0,0 +1,54 @@
+﻿using NicBell.UCreate.Helpers;
+using System;
+using System.Runtime.CompilerServices;
+using Umbraco.Core.Models;
+
+namespace NicBell.UCreate.Attributes
+{
+    /// <summary>
+    /// Makes property as part of the generated type
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class CustomTypePropertyAttribute : Attribute
+    {
+        public string Alias { get; set; }
+        public string Name { get;  private set; }
+        public string Description { get; set; }
+        public bool Mandatory { get; set; }
+        public string ValidationRegExp { get; set; }
+
+        public object DefaultValue { get; set; }
+        public string TypeName { get; set; }
+        public string TabName { get; set; }
+
+
+        /// <summary>
+        /// Set default values
+        /// </summary>
+        public CustomTypePropertyAttribute([CallerMemberName]string name = null)
+        {
+            this.Mandatory = false;
+            this.ValidationRegExp = "";
+            this.Description = "";
+            this.DefaultValue = null;
+            this.Alias = "";
+            this.TypeName = "Textstring";
+            this.Name = name;
+        }
+
+
+        public PropertyType GetPropertyType()
+        {
+            var propType = new PropertyType(new DataTypeHelper().GetDataType(TypeName))
+            {
+                Alias = Alias,
+                Name = Name,
+                Description = Description,
+                Mandatory = Mandatory,
+                ValidationRegExp = ValidationRegExp
+            };
+
+            return propType;
+        }
+    }
+}
---
+++ NicBell.UCreate/Constants/PropertyTypes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NicBell.UCreate.Constants
+{
+    public static class PropertyTypes
+    {
+        public const string ApprovedColor = "Approved Color";
+        public const string Checkboxlist = "Checkbox list";
+        public const string ContentPicker = "Content Picker";
+        public const string DatePickerwithtime = "Date Picker with time";
+        public const string DatePicker = "Date Picker";
+        public const string Dropdownmultiple = " Dropdown multiple";
+        public const string Dropdown = "Dropdown";
+        public const string FolderBrowser = "Folder Browser";
+        public const string Label = "Label";
+        public const string MediaPicker = "Media Picker";
+        public const string MemberPicker = "Member Picker";
+        public const string MultipleMediaPicker = " Multiple Media Picker";
+        public const string Numeric = "Numeric";
+        public const string Radiobox = "Radiobox";
+        public const string RelatedLinks = "Related Links";
+        public const string Richtexteditor = "Richtext editor";
+        public const string Tags = "Tags";
+        public const string Textboxmultiple = "Textbox multiple";
+        public const string Textstring = "Textstring";
+        public const string TrueFalse = "True/false";
+        public const string Upload = "Upload";
+    }
+}
