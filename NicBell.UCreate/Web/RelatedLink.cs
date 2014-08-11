d8e8dbd0b47d13efdbf9f9ad2566be2b23970e2b
Nic Bell <me@nicbell.net>
2014-08-11 17:32:58 +0100

    Custom type mapping.

---
+++ NicBell.UCreate/Web/RelatedLink.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace NicBell.UCreate.Web
+{
+    public class RelatedLink
+    {
+        public string Caption { get; set; }
+        public string Link { get; set; }
+        public bool NewWindow { get; set; }
+        public bool Edit { get; set; }
+        public bool IsInternal { get; set; }
+        public string Type { get; set; }
+        public string Title { get; set; }
+    }
+}
