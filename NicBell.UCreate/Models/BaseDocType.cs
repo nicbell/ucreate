10a22fdc54d579235d435541b7c93d7c735ef665
Nic Bell <nic.bell@hogarthww.com>
2015-06-04 18:11:39 +0100

    Made mapping published content to doctype lighter by checking if attributes are defined.

--- NicBell.UCreate/Models/BaseDocType.cs
+++ NicBell.UCreate/Models/BaseDocType.cs
@@ -1,6 +1,7 @@
 ﻿using NicBell.UCreate.Attributes;
 using System;
 using System.ComponentModel;
+using System.Linq;
 using System.Reflection;
 using Umbraco.Core;
 using Umbraco.Core.Models;
@@ -18,36 +19,38 @@ namespace NicBell.UCreate.Models
 
 
         /// <summary>
-        /// This is magic
+        /// Gets all properties with PropertyAttribute and sets their value
         /// </summary>
         /// <param name="content"></param>
         private void SetValues(IPublishedContent content)
         {
-            foreach (PropertyInfo property in GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
+            foreach (var property in GetType().GetProperties().Where(prop => Attribute.IsDefined(prop, typeof(PropertyAttribute), false)))
             {
-                PropertyAttribute propAttr = Attribute.GetCustomAttribute(property, typeof(PropertyAttribute), false) as PropertyAttribute;
+                var alias = property.GetCustomAttribute<PropertyAttribute>(false).Alias;
+                SetValue(property, content.GetProperty(alias).Value);
+            }
+        }
 
-                if (propAttr != null)
-                {
-                    var value = content.GetProperty(propAttr.Alias).Value;
 
-                    //Custom converter..
-                    var converterAttr = property.GetCustomAttribute<TypeConverterAttribute>();
-
-                    if (converterAttr != null)
-                    {
-                        var converter = Activator.CreateInstance(Type.GetType(converterAttr.ConverterTypeName)) as TypeConverter;
-                        property.SetValue(this, converter.ConvertFrom(value ?? String.Empty));
-                    }
-                    else
-                    {
-                        var convert = value.TryConvertTo(property.PropertyType);
-                        if (convert.Success)
-                        {
-                            property.SetValue(this, convert.Result);
-                        }
-                    }
-                }
+        /// <summary>
+        /// Set value for a property.
+        /// </summary>
+        /// <param name="property"></param>
+        /// <param name="value"></param>
+        private void SetValue(PropertyInfo property, object value)
+        {
+            //Check for type converter..
+            if (Attribute.IsDefined(property, typeof(TypeConverterAttribute)))
+            {
+                var converterAttr = property.GetCustomAttribute<TypeConverterAttribute>();
+                var converter = Activator.CreateInstance(Type.GetType(converterAttr.ConverterTypeName)) as TypeConverter;
+                property.SetValue(this, converter.ConvertFrom(value ?? String.Empty));
+            }
+            else
+            {
+                var convert = value.TryConvertTo(property.PropertyType);
+                if (convert.Success)
+                    property.SetValue(this, convert.Result);
             }
         }
     }
